d25726fea266b9dd76a1afb4e1ad99e2d69991fa
Luis Silva <lotusms@outlook.com>
2019-12-04 09:15:08 -0500

    no message

--- todo/src/router/routes.js
+++ todo/src/router/routes.js
@@ -2,18 +2,18 @@
 const routes = [
   {
     path: '/',
+    component: () => import('layouts/LoginRegisterLayout.vue'),
+    children: [
+      { path: '/auth', component: () => import('pages/PageAuth.vue') }
+    ]
+  },
+  {
+    path: '/todos',
     component: () => import('layouts/Layout.vue'),
     children: [
       { path: '/todos', component: () => import('pages/PageTodo.vue') },
       { path: '/settings', component: () => import('pages/PageSettings.vue') },
       { path: '/settings/help', component: () => import('pages/PageHelp.vue') }
-    ]
-  },
-  {
-    path: '/auth',
-    component: () => import('layouts/LoginRegisterLayout.vue'),
-    children: [
-      { path: '/auth', component: () => import('pages/PageAuth.vue') }
     ]
   }
 ]
